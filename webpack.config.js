850cb63897a2f26a18ce899154dcf77b225f7308
nimjakaaa <mohamed.nimjakaaa.kaabouchi@gmail.com>
2022-10-03 09:14:37 +0000

    Add and configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,16 @@ module.exports = {
         clean: true,
     },
     mode: 'production',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
     module: {
         rules: [
             {
