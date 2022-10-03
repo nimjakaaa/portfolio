b2ee21d322bd99bd67977a7d15c44f07fc1d1bb9
nimjakaaa <mohamed.nimjakaaa.kaabouchi@gmail.com>
2022-10-03 09:10:36 +0000

    Add and configure mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const path = require('path');
 
@@ -7,13 +8,27 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        clean: true,
     },
     mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg)$/i,
+                type: 'asset/ressource',
+            },
+        ],
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'My First Portfolio',
             filename: 'index.html',
             template: './src/template.html',
         }),
-    ]
+        new MiniCssExtractPlugin(),
+    ],
 }
