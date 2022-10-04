67e1ce1362a30c5281b58123a322d75fe7bfe49c
nimjakaaa <mohamed.nimjakaaa.kaabouchi@gmail.com>
2022-10-04 17:19:48 +0000

    Add about me

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import Header from './components/header/Header';
 import Socials from './components/socials/Socials';
 import EmailContact from './components/emailContact/EmailContact';
 import Showcase from './components/Showcase/Showcase';
+import AboutMe from './components/abouMe/AboutMe';
 
-import { headerRef, socialsRef, emailContactRef, showcaseRef } from './domRefs';
+import { headerRef, socialsRef, emailContactRef, showcaseRef, aboutMeRef } from './domRefs';
 import './styles.css';
 
 const App = () => {
     headerRef.innerHTML = Header();
-    showcaseRef.innerHTML += Showcase();
 
+    showcaseRef.innerHTML = Showcase();
+    aboutMeRef.innerHTML = AboutMe();
+    
 
     socialsRef.innerHTML = Socials();
     emailContactRef.innerHTML = EmailContact();
