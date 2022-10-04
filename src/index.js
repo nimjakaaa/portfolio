edb28ad46aa3c6c17e13c4b9ac9ee2d0e2f6870a
nimjakaaa <mohamed.nimjakaaa.kaabouchi@gmail.com>
2022-10-04 09:43:31 +0000

    Add Showcase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import Header from './components/header/Header';
 import Socials from './components/socials/Socials';
 import EmailContact from './components/emailContact/EmailContact';
+import Showcase from './components/Showcase/Showcase';
 
-import { headerRef, socialsRef, emailContact } from './domRefs';
+import { headerRef, socialsRef, emailContactRef, showcaseRef } from './domRefs';
 import './styles.css';
 
 const App = () => {
     headerRef.innerHTML = Header();
+    showcaseRef.innerHTML += Showcase();
+
+
     socialsRef.innerHTML = Socials();
-    emailContact.innerHTML = EmailContact();
+    emailContactRef.innerHTML = EmailContact();
 }
 
 App();
