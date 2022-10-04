6b93cdb6e01c4e1fc1f01122e23da8865300b9ff
nimjakaaa <mohamed.nimjakaaa.kaabouchi@gmail.com>
2022-10-04 18:53:44 +0000

    Add Contact me

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import Socials from './components/socials/Socials';
 import EmailContact from './components/emailContact/EmailContact';
 import Showcase from './components/Showcase/Showcase';
 import AboutMe from './components/abouMe/AboutMe';
-import Works from './components/Works/Works';
+import Works from './components/works/Works';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
-import { headerRef, socialsRef, emailContactRef, showcaseRef, aboutMeRef, worksRef } from './domRefs';
+import { headerRef, socialsRef, emailContactRef, showcaseRef, aboutMeRef, worksRef, contactRef, footerRef } from './domRefs';
 import './styles.css';
 
 const App = () => {
@@ -14,8 +16,9 @@ const App = () => {
     showcaseRef.innerHTML = Showcase();
     aboutMeRef.innerHTML = AboutMe();
     worksRef.innerHTML = Works();
+    contactRef.innerHTML = Contact();
 
-
+    footerRef.innerHTML = Footer();
     socialsRef.innerHTML = Socials();
     emailContactRef.innerHTML = EmailContact();
 }
