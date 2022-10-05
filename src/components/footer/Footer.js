210f2728b012f1f292a08aeb8550e3e04211a6e2
nimjakaaa <mohamed.nimjakaaa.kaabouchi@gmail.com>
2022-10-05 20:29:07 +0000

    Add source code link

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,6 +3,7 @@ import './Footer.css';
 const Footer = () => {
     const template = `
         <p>Designed & Built by Mohamed Kaabouchi</p>
+        <a href="https://github.com/nimjakaaa/portfolio.git"><span>Source Code</span></a>
     `;
 
     return template;
